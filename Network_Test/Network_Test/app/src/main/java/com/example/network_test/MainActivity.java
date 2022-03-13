eb645e63b5bf01be084e059841bb31ad64c762a5
ct222ct <claudiatam@edu.aau.at>
2022-03-13 13:22:27 +0100

    Implementation of the second task
    Verbindung zu Server über TCP-Client mittels Button "click".
    Antwort von Server wir in Textfeld angezeigt

--- Network_Test/Network_Test/app/src/main/java/com/example/network_test/MainActivity.java
+++ Network_Test/Network_Test/app/src/main/java/com/example/network_test/MainActivity.java
@@ -2,13 +2,111 @@ package com.example.network_test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Button verbinden;
+        EditText matrikelnummer;
+        TextView serverAntwort;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            matrikelnummer = (EditText) findViewById(R.id.editTextNumber);
+            serverAntwort = (TextView) findViewById(R.id.serverAntwort);
+            verbinden = (Button) findViewById(R.id.verbinden);
+
+            verbinden.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    new TCP_Client().execute();
+                    System.out.println("Verbunden");
+                }
+            });
+        }
+
+
+    class TCP_Client extends AsyncTask<Void, Void, Void> {
+
+        String antwort;
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            OutputStreamWriter outputStreamWriter = null;
+            InputStreamReader inputStreamReader = null;
+            BufferedReader bR = null;
+            BufferedWriter bW = null;
+            matrikelnummer = (EditText) findViewById(R.id.editTextNumber);
+
+            Socket socket = null;
+
+            try {
+                socket = new Socket("se2-isys.aau.at",53212);
+
+                inputStreamReader = new InputStreamReader(socket.getInputStream());
+                outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
+
+                bR = new BufferedReader(inputStreamReader);
+                bW = new BufferedWriter(outputStreamWriter);
+
+                bW.write(matrikelnummer.getText().toString());
+                bW.newLine();
+                bW.flush();
+
+                antwort = bR.readLine();
+
+                System.out.println("Server-Antwort: " + antwort);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                antwort = "UnknownHostException: " + e.toString();
+
+            } finally {
+                try {
+                    if(socket != null)
+                        socket.close();
+
+                    if(inputStreamReader != null)
+                        inputStreamReader.close();
+
+                    if(outputStreamWriter != null)
+                        outputStreamWriter.close();
+
+                    if(bR != null)
+                        bR.close();
+
+                    if(bW != null)
+                        bW.close();
+
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            //serverAntwort = antwort;
+
+            serverAntwort.setText(antwort);
+        }
     }
 }
